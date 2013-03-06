ea7d4e93801a9e7377c7335dcce1f1cb88c9f9d3
Simone <hello@simonewebdesign.it>
2013-03-06 15:17:57 +0100

    remove old import statements
    Signed-off-by: Simone <hello@simonewebdesign.it>

--- src/com/example/todolist/MainActivity.java
+++ src/com/example/todolist/MainActivity.java
@@ -23,9 +23,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.example.todolist.utils.JsonUtil;
-import com.example.todolist.utils.JsonUtil.*;
-
 public class MainActivity extends Activity {
 
 	private static final String TAG = "MainActivity";
